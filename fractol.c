8f50b2c4a3d81cc4306b31e02c733aa7d8349afd
MohamedMorabet <medmrbt5@gmail.com>
2025-02-15 13:37:11 +0100

    🗂️ Separated fract-ol and its utils into different files – keeping things organized! 🔧

--- fractol.c
+++ fractol.c
@@ -6,11 +6,95 @@
 /*   By: mel-mora <mel-mora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 17:27:32 by mel-mora          #+#    #+#             */
-/*   Updated: 2025/02/14 12:34:21 by mel-mora         ###   ########.fr       */
+/*   Updated: 2025/02/15 13:27:21 by mel-mora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+
+//fractol_init.c
+void	fractol_init(t_fractol *fractol)
+{
+	fractol->mlx_connection = mlx_init();
+	if (!fractol->mlx_connection)
+		exit(1); // do it later
+	fractol->mlx_window = mlx_new_window(fractol->mlx_connection,
+			WIDTH, HEIGHT, "Fract'ol");
+	if (!fractol->mlx_window)
+		mlx_destroy_image(fractol->mlx_connection, fractol->img.img);
+	fractol->img.img = mlx_new_image(fractol->mlx_connection, WIDTH, HEIGHT);
+	if (!fractol->img.img)
+	{
+		mlx_destroy_window(fractol->mlx_connection, fractol->mlx_window);
+		mlx_destroy_image(fractol->mlx_connection, fractol->img.img);
+		free(fractol->mlx_connection);
+		exit(1); // do it later
+	}
+	fractol->img.pixels = mlx_get_data_addr(fractol->img.img,
+			&fractol->img.bits_per_pixel,
+			&fractol->img.size_line, &fractol->img.endian);
+	events_init(fractol); // do it later
+	data_init(fractol);
+}
+
+void	fractol_render(t_fractol *fractol)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (y < HEIGHT)
+	{
+		x = 0;
+		while (x < WIDTH)
+		{
+			hundle_pixel(x, y, fractol);
+			x++;
+		}
+		y++;
+	}
+	mlx_put_image_to_window(fractol->mlx_connection, fractol->mlx_window,
+		fractol->img.img, 0, 0);
+}
+
+void	mandelbrot_or_julia(t_complex *z, t_complex *c, t_fractol *fractol)
+{
+	if (ft_strcmp(fractol->name, "mandelbort") == 0)
+	{
+		c->a = z->a;
+		c->i = z->i;
+	}
+	else if (ft_strcmp(fractol->name, "julia") == 0)
+	{
+		c->a = fractol->julia_x;
+		c->i = fractol->julia_y;
+	}
+}
+
+void	hundle_pixel(int x, int y, t_fractol *fractol)
+{
+	t_complex	z;
+	t_complex	c;
+	int			i;
+	int			color;
+
+	i = 0;
+	z.a = (map(x, -2, 2, WIDTH) * fractol->zoom) + fractol->shift_x;
+	z.i = (map(y, 2, -2, HEIGHT) * fractol->zoom) + fractol->shift_y;
+	mandelbrot_or_julia(&z, &c, fractol);
+	while (i < fractol->iterations)
+	{
+		z = sum_complex(square_complex(z), c);
+		if ((z.a * z.a) + (z.i * z.i) > fractol->escape_value)
+		{
+			color = map(i, BLACK, WHITE, fractol->iterations);
+			my_mlx_pixel_put(&fractol->img, x, y, color);
+			return ;
+		}
+		i++;
+	}
+	my_mlx_pixel_put(&fractol->img, x, y, PSYCHEDELIC_PURPLE);
+}
 
 void	my_mlx_pixel_put(t_img *data, int x, int y, int color)
 {
---
+++ fractol_utils.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol_utils.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-mora <mel-mora@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/15 12:31:04 by mel-mora          #+#    #+#             */
+/*   Updated: 2025/02/15 13:27:49 by mel-mora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+void	data_init(t_fractol *fractol)
+{
+	fractol->escape_value = 4;
+	fractol->iterations = 42;
+	fractol->shift_x = 0.0;
+	fractol->shift_y = 0.0;
+	fractol->zoom = 1.0;
+}
+
+void	events_init(t_fractol *fractol)
+{
+	mlx_hook(fractol->mlx_window, KEYPRESS, 0, key_handler, fractol);
+	mlx_hook(fractol->mlx_window, BUTTONPRESS, 0, mouse_handler, fractol);
+	mlx_hook(fractol->mlx_window, DESTROYNOTIFY, 0, close_handler, fractol);
+	mlx_hook(fractol->mlx_window, MONTIONNOTIFY, 0, mouse_track, fractol);
+}
