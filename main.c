efb77f2474f1508d257baa5b78abc7a2fb191994
MohamedMorabet <medmrbt5@gmail.com>
2025-02-15 13:39:03 +0100

    🎨 Fixed norm and organized headers – keeping it clean and structured! ✅

--- main.c
+++ main.c
@@ -6,95 +6,11 @@
 /*   By: mel-mora <mel-mora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 09:42:42 by mel-mora          #+#    #+#             */
-/*   Updated: 2025/02/14 22:32:16 by mel-mora         ###   ########.fr       */
+/*   Updated: 2025/02/15 13:30:23 by mel-mora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
-
-void	data_init(t_fractol *fractol)
-{
-	fractol->escape_value = 4;
-	fractol->iterations = 42;
-	fractol->shift_x = 0.0;
-	fractol->shift_y = 0.0;
-	fractol->zoom = 1.0;
-}
-
-void	events_init(t_fractol *fractol)
-{
-	mlx_hook(fractol->mlx_window, KEYPRESS, 0, key_handler, fractol);
-	mlx_hook(fractol->mlx_window, BUTTONPRESS, 0, mouse_handler, fractol);
-	mlx_hook(fractol->mlx_window, DESTROYNOTIFY, 0, close_handler, fractol);
-}
-
-//fractol_init.c
-void fractol_init(t_fractol *fractol)
-{
-	fractol->mlx_connection = mlx_init();
-	if (!fractol->mlx_connection)
-		exit(1); // do it later
-	fractol->mlx_window = mlx_new_window(fractol->mlx_connection, WIDTH, HEIGHT, "Fract'ol");
-	if (!fractol->mlx_window)
-		mlx_destroy_image(fractol->mlx_connection, fractol->img.img);
-	fractol->img.img = mlx_new_image(fractol->mlx_connection, WIDTH, HEIGHT);
-	if (!fractol->img.img)
-	{
-		mlx_destroy_window(fractol->mlx_connection, fractol->mlx_window);
-		mlx_destroy_image(fractol->mlx_connection, fractol->img.img);
-		free(fractol->mlx_connection);
-		exit(1); // do it later
-	}
-	fractol->img.pixels = mlx_get_data_addr(fractol->img.img, &fractol->img.bits_per_pixel, &fractol->img.size_line, &fractol->img.endian);
-
-	events_init(fractol); // do it later
-	data_init(fractol);
-}
-
-void	hundle_pixel(int x, int y, t_fractol *fractol)
-{
-	t_complex	z;
-	t_complex	c;
-	int 		i;
-	int 		color;
-
-	i = 0;
-	z.a = 0.0;
-	z.i = 0.0;
-	c.a = (map(x, -2, 2, 0, WIDTH) * fractol->zoom) + fractol->shift_x;
-	c.i = (map(y, 2, -2, 0, HEIGHT) * fractol->zoom) + fractol->shift_y;
-	while (i < fractol->iterations)
-	{
-		z = sum_complex(square_complex(z), c);
-		if ((z.a * z.a) + (z.i * z.i) > fractol->escape_value)
-		{
-			color = map(i, BLACK, WHITE, 0, fractol->iterations);
-			my_mlx_pixel_put(&fractol->img, x, y, color);
-			return ;
-		}
-		i++;
-	}
-	my_mlx_pixel_put(&fractol->img, x, y, PSYCHEDELIC_PURPLE);
-}
-
-void	fractol_render(t_fractol *fractol)
-{
-	int x;
-	int y;
-
-	y = 0;
-	while (y < HEIGHT)
-	{
-		x = 0;
-		while (x < WIDTH)
-		{
-			hundle_pixel(x, y, fractol);
-			x++;
-		}
-		y++;
-	}
-	mlx_put_image_to_window(fractol->mlx_connection, fractol->mlx_window, fractol->img.img, 0, 0);
-}
 
 // check leaks
 void	leaks()
@@ -102,23 +18,30 @@ void	leaks()
 	system("leaks fractol");
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-	t_fractol fractol;
+	t_fractol	fractol;
 
 	// check for leaks
 	// atexit(leaks);
-    if ((ac == 2 && ft_strcmp(av[1], "mandelbort") == 0)
-     || (ac == 4 && ft_strcmp(av[1], "julia") == 0))
-    {
+	if ((ac == 2 && ft_strcmp(av[1], "mandelbort") == 0)
+		|| (ac == 4 && ft_strcmp(av[1], "julia") == 0))
+	{
 		fractol.name = av[1];
+		if (ac == 4)
+		{
+			fractol.julia_x = atof(av[2]);
+			fractol.julia_y = atof(av[3]);
+		}
 		fractol_init(&fractol);
-		printf("\033[1;32m🚀 Fract'ol is starting... Let's create some fractal magic! ✨\033[0m\n");
+		printf("\033[1;32m🚀 Fract'ol is starting... Let's create"
+			" some fractal magic! ✨\033[0m\n");
 		fractol_render(&fractol);
-		printf("\033[1;34m🎨 Fract'ol is rendering... Sit back and enjoy the beauty! 🌌\033[0m\n");
+		printf("\033[1;34m🎨 Fract'ol is rendering... Sit back and"
+			" enjoy the beauty! 🌌\033[0m\n");
 		mlx_loop(fractol.mlx_connection);
 	}
 	else
-        its_error(ac, av);
-    return (0);
-} 
+		its_error(ac, av);
+	return (0);
+}
--- utils.c
+++ utils.c
@@ -6,60 +6,68 @@
 /*   By: mel-mora <mel-mora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 09:41:24 by mel-mora          #+#    #+#             */
-/*   Updated: 2025/02/14 11:29:17 by mel-mora         ###   ########.fr       */
+/*   Updated: 2025/02/15 13:31:50 by mel-mora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-// ft_strcmp.c
-int ft_strcmp(char *s1, char *s2)
+int	ft_strcmp(char *s1, char *s2)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (s1[i] && s2[i] && s1[i] == s2[i])
-        i++;
-    return (s1[i] - s2[i]);
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
 }
 
-// putstr_fd.c
-void    ft_putstr_fd(char *s, int fd)
+void	ft_putstr_fd(char *s, int fd)
 {
-    int i;
-    
-    i = 0;
-    if (!s || fd < 0)
-        return ;
-    while (s[i])
-    {
-        write(fd, &s[i], 1);
-        i++;
-    }
+	int	i;
+
+	i = 0;
+	if (!s || fd < 0)
+		return ;
+	while (s[i])
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
 }
 
-// math_utils.c
-double  map(double value, double new_min, double new_max, double old_min, double old_max)
+static int	len_num(double num)
 {
-    return (new_min + (value - old_min) * (new_max - new_min) / (old_max - old_min));
+	int	len;
+
+	len = 0;
+	while (num > 1)
+	{
+		num /= 10;
+		len++;
+	}
+	return (len);
 }
 
-// sun complex number
-t_complex   sum_complex(t_complex a, t_complex b)
+double	atodbl(char *str)
 {
-    t_complex res;
+	double	res;
+	double	dec;
+	int		sign;
+	int		i;
 
-    res.a = a.a + b.a;
-    res.i = a.i + b.i;
-    return (res);
-}
-
-// square complex number
-t_complex   square_complex(t_complex a)
-{
-    t_complex res;
-
-    res.a = a.a * a.a - a.i * a.i;
-    res.i = 2 * a.a * a.i;
-    return (res);
+	res = 0;
+	dec = 0;
+	sign = 1;
+	i = 0;
+	if (str[i++] == '-')
+		sign = -1;
+	while (str[i] && str[i] != '.')
+		res = res * 10 + str[i++] - '0';
+	if (str[i++] == '.')
+	{
+		while (str[i])
+			dec = dec * 10 + str[i++] - '0';
+	}
+	return (sign * (res + dec / pow(10, len_num(dec))));
 }
