01e8b224856ed7c4eda34a6822c26a9d2fb0c4ce
MohamedMorabet <medmrbt5@gmail.com>
2025-02-26 22:32:34 +0000

    🧹 Removed some bonus events – keeping things clean and focused! 🔧

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: mel-mora <mel-mora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 17:26:40 by mel-mora          #+#    #+#             */
-/*   Updated: 2025/02/15 12:57:06 by mel-mora         ###   ########.fr       */
+/*   Updated: 2025/02/26 20:45:32 by mel-mora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,23 +26,14 @@
 # define BUTTONPRESS 4
 # define DESTROYNOTIFY 17
 # define MONTIONNOTIFY 6
+# define ESC_KEY 53
+# define PLUS_KEY 69
+# define MINUS_KEY 78
 
 // Colors
-# define BLACK	0x000000 // RGB(0, 0, 0)
-# define WHITE	0xFFFFFF // RGB(255, 255, 255)
-# define RED		0xFF0000 // RGB(255, 0, 0)
-# define GREEN	0x00FF00 // RGB(0, 255, 0)
-# define BLUE	0x0000FF // RGB(0, 0, 255)
-
-// Psychedelic colors
-# define MAGENTA_BURST		0xFF00FF  // A vibrant magenta
-# define LIME_SHOCK			0xCCFF00  // A blinding lime
-# define NEON_ORANGE			0xFF6600  // A blazing neon orange
-# define PSYCHEDELIC_PURPLE	0x660066  // A deep purple
-# define AQUA_DREAM			0x33CCCC  // A bright turquoise
-# define HOT_PINK			0xFF66B2  // As the name suggests!
-# define ELECTRIC_BLUE		0x0066FF  // A radiant blue
-# define LAVA_RED			0xFF3300  // A bright, molten red
+# define BLACK	0x000000
+# define WHITE	0xFFFFFF
+# define PSYCHEDELIC_PURPLE	0x660066
 
 //image struct
 typedef struct s_img
@@ -99,7 +90,6 @@ void		fractol_render(t_fractol *fractol);
 /*-------------------- Event Handling Functions --------------------*/
 int			key_handler(int keysym, t_fractol *fractol);
 int			mouse_handler(int button, int x, int y, t_fractol *fractol);
-int			mouse_track(int x, int y, t_fractol *fractol);
 int			close_handler(t_fractol *fractol);
 
 /*-------------------- Initialization Functions --------------------*/
--- main.c
+++ main.c
@@ -6,32 +6,24 @@
 /*   By: mel-mora <mel-mora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 09:42:42 by mel-mora          #+#    #+#             */
-/*   Updated: 2025/02/15 13:30:23 by mel-mora         ###   ########.fr       */
+/*   Updated: 2025/02/26 22:16:24 by mel-mora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-// check leaks
-void	leaks()
-{
-	system("leaks fractol");
-}
-
 int	main(int ac, char **av)
 {
 	t_fractol	fractol;
 
-	// check for leaks
-	// atexit(leaks);
-	if ((ac == 2 && ft_strcmp(av[1], "mandelbort") == 0)
+	if ((ac == 2 && ft_strcmp(av[1], "mandelbrot") == 0)
 		|| (ac == 4 && ft_strcmp(av[1], "julia") == 0))
 	{
 		fractol.name = av[1];
 		if (ac == 4)
 		{
-			fractol.julia_x = atof(av[2]);
-			fractol.julia_y = atof(av[3]);
+			fractol.julia_x = atodbl(av[2]);
+			fractol.julia_y = atodbl(av[3]);
 		}
 		fractol_init(&fractol);
 		printf("\033[1;32m🚀 Fract'ol is starting... Let's create"
